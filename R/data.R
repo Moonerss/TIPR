c55be31b5d154baf7244c42fb0f80d1390439738
Moonerss <2055469819@qq.com>
2024-09-26 22:01:04 +0800

    add signature data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' cancer-immunity cycle related gene set
+#'
+#' @description A \code{list} of gene set vector get from TIP web server, all symbol
+#' have checked use \code{HGNChelper} package.
+#'
+#' @examples
+#' data("TIP_signature_symbol", package="TIPR")
+#' head(TIP_signature_symbol)
+#'
+#' @source
+#' TIP web server \url{http://biocc.hrbmu.edu.cn/TIP/index.jsp}
+#'
+"TIP_signature_symbol"
